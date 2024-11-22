a7426ef6d816d30fd8fc328bc7710ec7127171ee
Jeppe Olsen <Olsen1906@gmail.com>
2024-11-22 15:36:16 +0100

    Dataseeding file 2.0.

---
+++ DataSeedingSqlFile.sql
@@ -0,0 +1,51 @@
+﻿-- En måde vi kan generere Insert statements til vores tabeller. 
+-- Bruges kun til at generere nye insert statements, slet hvis du ikke skal bruge dem.
+SELECT 
+    'INSERT INTO Photographers (Column1, Column2, Column3) VALUES (' +
+    QUOTENAME(PhotographerId, '''') + ', ' +
+    QUOTENAME(Name, '''') + ');'
+FROM Photographers;
+
+SELECT 
+    'INSERT INTO Albums (Column1, Column2, Column3) VALUES (' +
+    QUOTENAME(AlbumId, '''') + ', ' +
+    QUOTENAME(Name, '''') + ', ' +
+    QUOTENAME(Deadline, '''') + ', ' +
+    QUOTENAME(AlbumLink, '''') + ', ' +
+    QUOTENAME(PhotographerId, '''') + ');'
+FROM Albums;
+
+SELECT 
+    'INSERT INTO Customers (Column1, Column2, Column3) VALUES (' +
+    QUOTENAME(CustomerId, '''') + ', ' +
+    QUOTENAME(Name, '''') + ', ' +
+    QUOTENAME(Email, '''') + ', ' +
+    QUOTENAME(Phone, '''') + ', ' +
+    QUOTENAME(AlbumId, '''') + ');'
+FROM Customers;
+
+
+-- Insert statements
+
+Begin transaction;
+
+SET IDENTITY_INSERT Photographers ON;
+INSERT INTO Photographers (Column1, Column2, Column3) VALUES ('1', 'Ben');
+INSERT INTO Photographers (Column1, Column2, Column3) VALUES ('2', 'Hans');
+INSERT INTO Photographers (Column1, Column2, Column3) VALUES ('3', 'Kalle');
+INSERT INTO Photographers (Column1, Column2, Column3) VALUES ('4', 'Henning');
+SET IDENTITY_INSERT Photographers OFF;
+
+SET IDENTITY_INSERT Albums ON;
+INSERT INTO Albums (Column1, Column2, Column3) VALUES ('11', 'Solsikken', '2025-05-16 05:50:06.0000000', 'www.juhuuu.com', '1');
+INSERT INTO Albums (Column1, Column2, Column3) VALUES ('12', 'Bavian', '2027-12-02 14:00:00.0000000', 'www.bavian.com', '4');
+INSERT INTO Albums (Column1, Column2, Column3) VALUES ('17', 'Nike', '2025-05-16 13:00:00.0000000', 'www.Nike.com', '1');
+SET IDENTITY_INSERT Albums OFF;
+
+SET IDENTITY_INSERT Customers ON;
+INSERT INTO Customers (Column1, Column2, Column3) VALUES ('1', 'Nike', 'nike@gmail.com', '12121212', '17');
+INSERT INTO Customers (Column1, Column2, Column3) VALUES ('2', 'Janne', 'janne@gmail.com', '28367374', '11');
+INSERT INTO Customers (Column1, Column2, Column3) VALUES ('3', 'Tøjbutikken', 'Tøj@gmail.com', '65653355', '12');
+SET IDENTITY_INSERT Customers OFF;
+
+Commit transaction;
