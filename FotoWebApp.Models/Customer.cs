bb9ac30cf49fd2f12d25a81a965500c9ff38fcde
TrisES <112662675+TrisES@users.noreply.github.com>
2024-11-26 15:20:24 +0100

    WIP: Added some initial validation/requirements on our model classes.

--- FotoWebApp.Models/Customer.cs
+++ FotoWebApp.Models/Customer.cs
@@ -14,8 +14,14 @@ namespace FotoWebApp.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // This attribute specifies that the database will generate the primary key value.
         public int CustomerId { get; set; }
 
+        [Required] // This attribute specifies that the property is required.
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")] // This attribute specifies the maximum and minimum length of the property.
         public string Name { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Email must be between 1 and 50 characters")]
         public string Email { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Phone must be between 8 and 20 characters")]
         public string Phone { get; set; }
 
 
