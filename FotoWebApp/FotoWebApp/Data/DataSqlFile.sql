e48efbd63cf15583c2befdbd1fb84f21de18e983
TrisES <112662675+TrisES@users.noreply.github.com>
2024-12-04 18:27:12 +0100

    Update Image model and fix Migrations. Improve and update DataSQLFile script.

--- FotoWebApp/FotoWebApp/Data/DataSqlFile.sql
+++ FotoWebApp/FotoWebApp/Data/DataSqlFile.sql
@@ -27,24 +27,29 @@ FROM Customers;
 
 
 --Insert statements
-Begin transaction;
+BEGIN TRY
+    BEGIN TRANSACTION;
 
-SET IDENTITY_INSERT Photographers ON;
-INSERT INTO Photographers (PhotographerId, Name) VALUES ('1', 'Ben');
-INSERT INTO Photographers (PhotographerId, Name) VALUES ('2', 'Hans');
-INSERT INTO Photographers (PhotographerId, Name) VALUES ('3', 'Kalle');
-INSERT INTO Photographers (PhotographerId, Name) VALUES ('4', 'Henning');
-SET IDENTITY_INSERT Photographers OFF;
+    SET IDENTITY_INSERT Photographers ON;
+    INSERT INTO Photographers (PhotographerId, Name) VALUES ('1', 'Ben');
+    INSERT INTO Photographers (PhotographerId, Name) VALUES ('2', 'Hans');
+    INSERT INTO Photographers (PhotographerId, Name) VALUES ('3', 'Kalle');
+    INSERT INTO Photographers (PhotographerId, Name) VALUES ('4', 'Henning');
+    SET IDENTITY_INSERT Photographers OFF;
 
-SET IDENTITY_INSERT Albums ON;
-INSERT INTO Albums (AlbumId, Name, Deadline, AlbumLink, PhotographerId, MaxSelectedImages) VALUES ('12', 'Bavians Juletrøjer', '2027-12-02 14:00:00.0000000', 'www.bavian.com', '4', '100');
-INSERT INTO Albums (AlbumId, Name, Deadline, AlbumLink, PhotographerId, MaxSelectedImages) VALUES ('17', 'Nike', '2025-05-16 13:00:00.0000000', 'www.Nike.com', '1', '50');
-SET IDENTITY_INSERT Albums OFF;
+    SET IDENTITY_INSERT Albums ON;
+    INSERT INTO Albums (AlbumId, Name, Deadline, AlbumLink, PhotographerId, MaxSelectedImages) VALUES ('12', 'Bavians Juletrøjer', '2027-12-02 14:00:00.0000000', 'www.bavian.com', '4', '100');
+    INSERT INTO Albums (AlbumId, Name, Deadline, AlbumLink, PhotographerId, MaxSelectedImages) VALUES ('17', 'Nike', '2025-05-16 13:00:00.0000000', 'www.Nike.com', '1', '50');
+    SET IDENTITY_INSERT Albums OFF;
 
-SET IDENTITY_INSERT Customers ON;
-INSERT INTO Customers (CustomerId, Name, Email, Phone, AlbumId) VALUES ('1', 'Nike', 'nike@gmail.com', '12121212', '17');
-INSERT INTO Customers (CustomerId, Name, Email, Phone, AlbumId) VALUES ('2', 'Janne', 'janne@gmail.com', '28367374', '11');
-INSERT INTO Customers (CustomerId, Name, Email, Phone, AlbumId) VALUES ('3', 'Tøjbutikken', 'Tøj@gmail.com', '65653355', '12');
-SET IDENTITY_INSERT Customers OFF;
+    SET IDENTITY_INSERT Customers ON;
+    INSERT INTO Customers (CustomerId, Name, Email, Phone, AlbumId) VALUES ('1', 'Nike', 'nike@gmail.com', '12121212', '17');
+    INSERT INTO Customers (CustomerId, Name, Email, Phone, AlbumId) VALUES ('2', 'Janne', 'janne@gmail.com', '28367374', '12');
+    SET IDENTITY_INSERT Customers OFF;
 
-Commit transaction;
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+    THROW;
+END CATCH;
