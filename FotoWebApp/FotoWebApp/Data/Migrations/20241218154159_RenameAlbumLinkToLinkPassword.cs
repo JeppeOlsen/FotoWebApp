52926949270a780d29c0a25706b16301b043f14a
TrisES <112662675+TrisES@users.noreply.github.com>
2024-12-18 17:03:44 +0100

    Update album model and add migration: - renamed AlbumLink prop to LinkPassword

---
+++ FotoWebApp/FotoWebApp/Data/Migrations/20241218154159_RenameAlbumLinkToLinkPassword.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FotoWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameAlbumLinkToLinkPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AlbumLink",
+                table: "Albums",
+                newName: "LinkPassword");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LinkPassword",
+                table: "Albums",
+                newName: "AlbumLink");
+        }
+    }
+}
